258790aa453a3771d9539571d01b204291772d57
ZZH <1462207500@qq.com>
2022-11-24 09:24:27 +0000

    增加fft实现, 增加fft和dft性能对比测试案例
    Signed-off-by: ZZH <1462207500@qq.com>

---
+++ benchmark.c
@@ -0,0 +1,40 @@
+/*
+@file: benchmark.c
+@author: ZZH
+@date: 2022-11-24
+@info: fft和dft性能差异对比, 此程序可能会运行长达几十秒, 取决于N的大小, 因为dft是非常低效的算法
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "fft.h"
+
+#define N 16384//此N值在我的电脑上, dft需要22秒多才能算出结果, 但同等条件fft仅需3毫秒
+#define FS 5000
+
+int main(const int argc, const char** argv)
+{
+    //构建一个含有多频率成分的信号
+    number_t input[N];
+    for (int i = 0;i < N;i++)
+    {
+        double arg = 2 * M_PI * i / FS;
+        input[i] = sin(70 * arg) + sin(800 * arg) + sin(2200 * arg);
+    }
+
+    Complex_t out1[N], out2[N];
+
+    clock_t dftStart = clock();
+    dft(out1, input, N);
+    clock_t dftEnd = clock();
+
+    clock_t fftStart = clock();
+    fft(out2, input, N);
+    clock_t fftEnd = clock();
+
+    printf("dft cost: %d, fft cost: %d\r\n", dftEnd - dftStart, fftEnd - fftStart);
+
+    return 0;
+
+}
--- fft.c
+++ fft.c
@@ -5,11 +5,113 @@
 @info: fft实现
 */
 #include "fft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 const double PI_2 = 2 * M_PI;
 
+//二分法找到第一个为1的二进制位, 根据size_t的大小可做到32位64位自适应
+static __attribute__((pure)) uint8_t ff1(size_t num)
+{
+    if (0 == num)
+        return 0;
+
+    uint8_t bitsOfVar = sizeof(num) * 8;//通过sizeof实现自适应
+    uint8_t index = 1;
+
+    //这里执行的时候变量的位数已经固定了, 所以实际上是O(1), 32位固定循环5次, 64位固定循环6次
+    while (bitsOfVar)
+    {
+        bitsOfVar /= 2;
+        size_t tmp = num >> bitsOfVar;
+        if (tmp)
+        {
+            index += bitsOfVar;
+            num = tmp;
+        }
+    }
+
+    return index;
+}
+
+//位倒序
+static __attribute__((pure)) size_t reverseBits(size_t num, uint8_t bitsOfVar)
+{
+    if (0 == num || bitsOfVar > (sizeof(num) * 8))
+        return 0;
+
+    const size_t topBit = 1 << (bitsOfVar - 1);
+    bitsOfVar >>= 1;
+
+    for (uint8_t i = 0;i < bitsOfVar;i++)
+    {
+        size_t lowMask = 1 << i;
+        size_t highMask = topBit >> i;
+        uint8_t lowBit = (num & lowMask) > 0;
+        uint8_t highBit = (num & highMask) > 0;
+
+        num &= ~(highMask | lowMask);
+        num |= (highMask * lowBit) | (lowMask * highBit);
+    }
+
+    return num;
+}
+
 void fft(Complex_t* output, number_t* input, size_t len)
 {
+    if (len < 2)
+        return;
+
+    uint8_t binLen = ff1(len) - 1;//首位二进制1的下标相当于求某个数的二进制长度, 例如1024的二进制长度是10
+    if (len & (len - 1))//计算长度如果不是2的N次幂
+        len = 1 << binLen;//求下一级的变换, 例如1023长度这里会当做512点数去计算, 1025会当做1024点数
+
+    //重排序
+    for (size_t i = 0;i < len;i++)
+    {
+        size_t rIndex = reverseBits(i, binLen);
+        //交换顺序的同时输出到output, 后续在output上原地进行蝶形运算
+        output[rIndex].real = input[i];
+        output[rIndex].image = 0;
+        output[i].real = input[rIndex];
+        output[i].image = 0;
+    }
+
+    //进行蝶形运算
+    //WpN = e^-i*PI_2*p/N = cos(PI_2*p/N) - i*sin(PI_2*p/N) (i为虚数单位)
+    //蝶形运算为 A=A+B*WpN B=A-B*WpN (A,B 均为复数,且等号左侧的A,B为下次迭代的值, 等号右侧的为本次的值)
+    for (size_t i = 0;i < binLen;i++)
+    {
+        size_t step = 1 << i;//数据间隔, 每层蝶形运算取数的间隔不同
+
+        //相同旋转因子的数量和数据间隔是一致的, 比如第一层全是W0N(p=0)
+        for (size_t j = 0;j < step;j++)//循环计算所有旋转因子
+        {
+            size_t k = 1 << binLen - i - 1;//旋转因子增量, 比如 W0N 到 W2N 增量为2
+            size_t p = j * k;//第j个旋转指数
+
+            number_t arg = PI_2 * p / len;
+            number_t sv = sin(arg);
+            number_t cv = cos(arg);
+
+            for (size_t z = 0;z < k;z++)//
+            {
+                size_t wStep = j + 2 * step * z;
+                //参与蝶形运算的实际就三个数, A B W, 其中A和B都是上一次迭代的输出值, W是旋转因子, 也就是那个展开是sin cos的公式
+                pComplex_t pA = &output[wStep], pB = pA + step;
+
+                //这个就相当于是B*WpN, tr结果的实数部分, ti是结果的虚数部分
+                number_t tr = pB->real * cv + pB->image * sv;
+                number_t ti = pB->image * cv - pB->real * sv;
+
+                //蝶形运算是A=A+B*WpN B=A-B*WpN,所以这里还得把A加进来
+                pB->real = pA->real - tr;
+                pB->image = pA->image - ti;
+                pA->real = pA->real + tr;
+                pA->image = pA->image + ti;
+            }
+        }
+    }
 }
 
 void ifft(number_t* output, Complex_t* input, size_t len)
